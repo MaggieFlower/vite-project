9c1a76063f038f436fc87967e34556acd7ef39a8
MaggieFlower <ben494675608@yeah.net>
2022-04-26 08:53:36 +0800

    feat: dialog组件

---
+++ src/components/dialog/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="mg-dialog">
+        <h5 class="mg-dialog--title">{{ props.title }}</h5>
+        <div class="mg-dialog--content">{{ props.content }}</div>
+    </section>
+</template>
+<script setup lang='ts'>
+import { defineProps } from 'vue';
+
+interface Props {
+    title: string;
+    content: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    title: '弹窗',
+    content: '这是一个弹窗',
+});
+</script>
+
+<style lang="scss">
+@import '@/styles/bem.scss';
+@include b('dialog') {
+    position: fixed;
+    right: 0px;
+    top: 0px;
+    width: 300px;
+    height: 150px;
+    &--title {
+        text-align: left;
+    }
+}
+</style>
--- src/pages/layout/sidebar.vue
+++ src/pages/layout/sidebar.vue
@@ -1,35 +1,27 @@
 <template>
-    <nav class="container-menu">
-        <template v-for="route in allPath"  :key="route.name">
-          <router-link  :to="route.path">{{route.name}}</router-link>
-          <br />
-        </template>
-<!-- 
-        <router-link to="/marketing">营销页面</router-link>
-        <br/>
-        <router-link to="/layout">mg布局</router-link>
-         <br/>
-        <router-link to="/transform">动画</router-link>
-        <br/>
-        <router-link to="/transform">表单</router-link> -->
-    </nav>
+  <nav class="container-menu">
+    <router-link
+      v-for="route in allPath"
+      :key="route.name"
+      :to="route.path"
+    >{{route.name}}</router-link>
+  </nav>
 </template>
 <script setup lang="ts">
-import {routes} from '@/src/router/routes.ts';
+import { routes } from '@/router/routes.ts';
 import { RouteRecordRaw } from 'vue-router';
 
-
-const allPath = routes.filter((route:RouteRecordRaw) => route.path);
+const allPath = routes.filter((route: RouteRecordRaw) => route.path);
 </script>
 <style lang="less">
-.container-menu{
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  flex-basis: 150px;
-  min-width: 150px;
-  height: 100%;
-  background-color: #f0f2f5;
+.container-menu {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    flex-basis: 150px;
+    min-width: 150px;
+    height: 100%;
+    background-color: #f0f2f5;
 }
 </style>
